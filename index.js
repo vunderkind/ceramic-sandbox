fc56e06eeb5c9d2ab94a2b5ca138787eb33be280
Justin Irabor <justin.irabor@gmail.com>
2022-04-01 23:39:14 +0100

    add did resolver

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const { CeramicClient } = require('@ceramicnetwork/http-client');
 const KeyDidResolver = require('key-did-resolver');
 const ThreeIdResolver = require('@ceramicnetwork/3id-did-resolver');
+const { DID } = require('dids');
 
 // const LOCAL_API_URL = 'http://localhost:7007';
 const FREE_API_URL = 'https://gateway-clay.ceramic.network';
 
-const CERAMIC = new CeramicClient(FREE_API_URL);
+const ceramic = new CeramicClient(FREE_API_URL);
+
+const resolver = {
+    ...KeyDidResolver.getResolver(),
+    ...ThreeIdResolver.getResolver(ceramic),
+};
+
+const did = new DID({ resolver });
+
+ceramic.did = did;
+
